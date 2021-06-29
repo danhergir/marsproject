e25e664ed96be54027d8ce0b1cb8457201cedec3
Aaron Whittier <aaronwhittier@outlook.com>
2021-06-28 18:45:28 -0700

    Updated news template
    Updated newsfeed template for displaying news

--- newsfeed/views.py
+++ newsfeed/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import requests
-import webbrowser
 import feedparser
 
 # Create your views here.
@@ -10,17 +9,15 @@ import feedparser
 
 def index(request):
     # parse xml feed
-    newsfeed = feedparser.parse("https://www,sciencedaily.com/rss/space_time/mars.xml")
+    newsfeed = feedparser.parse("https://www.sciencedaily.com/rss/space_time/mars.xml")
 
-    news = {}
-    entry_name = 'entry'
-    count = 0
+    # News list for storage.
+    news = []
 
+    # Store the rss feed items to the news list.
     for entry in newsfeed.entries:
         items = {'title': entry.title, 'link': entry.link, 'date': entry.published,
                  'time': entry.published_parsed, 'summary': entry.summary}
         news.append(items)
 
-        count += 1
-
-    return render(request, 'newsfeed.html', {'news': news})
+    return render(request, 'news.html', {'news': news})
